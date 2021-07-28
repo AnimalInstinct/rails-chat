cba7fbcd6e076e6b2f743046c8c79ad66cf7a11c
Alexander <a.makarov@molinos.ru>
2021-07-28 11:18:32 +0700

    bootstrap + icons + jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,14 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
-import "@doabit/semantic-ui-sass";
-import "@doabit/semantic-ui-sass/src/scss/semantic-ui.scss";
+import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+
+$(document).on("turbolinks:load", function () {
+  // console.log("Hello");
+  // $(".ui.dropdown").dropdown();
+});
