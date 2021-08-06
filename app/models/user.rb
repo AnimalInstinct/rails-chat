86a20b7086142e601cc1de2a283e6d2640fed48f
Alexander <a.makarov@molinos.ru>
2021-08-06 10:22:38 +0700

    authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_many :messages, dependent: :destroy
-  validates :username, presence: true, length: { minimum: 3, maximum: 15 }, uniqueness: true
+  before_save { self.email = email.downcase }
+  validates :username, presence: true,
+                       length: { minimum: 3, maximum: 25 },
+                       uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: { maximum: 105 },
+                    uniqueness: true,
+                    format: { with: VALID_EMAIL_REGEX }
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_032056) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
